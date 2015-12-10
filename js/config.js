f2ada0ffa0bfda223372dfa37ebf7373af9b9542
Edwin Ang <edwin@theroyalstudent.com>
2015-12-10 17:00:02 +0800

    Fixed date
    Signed-off-by: Edwin Ang <edwin@theroyalstudent.com>

--- js/config.js
+++ js/config.js
@@ -2,6 +2,6 @@ var configuration = {
 	name: 'Tesla OS', // required, string
 	endpointURL: 'https://teslaos.trackstats.space', // required, string
 	homepageURL: 'https://teslaos.github.io', // optional, string
-	startDate: '1434866400', // optional, string
+	startDate: 'Sun, 21 Jun 2015 06:00:00 GMT', // optional, string
 	updateInterval: 1500, // optional, integer
 }
