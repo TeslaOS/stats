678def0cc3db94403b0c8be23343a6bb0d565ffe
Edwin A <edwin@theroyalstudent.com>
2016-09-30 20:08:28 +0800

    Putting TrackStats offline.
    Signed-off-by: Edwin A <edwin@theroyalstudent.com>

--- js/config.js
+++ js/config.js
@@ -1,6 +1,6 @@
 var configuration = {
 	name: 'Tesla OS', // required, string
-	endpointURL: 'https://teslaos.trackstats.space', // required, string
+	endpointURL: 'https://teslaos.github.io/stats/stat.json', // required, string
 	homepageURL: 'https://teslaos.github.io', // optional, string
 	startDate: 'Sun, 21 Jun 2015 06:00:00 GMT', // optional, string
 	updateInterval: 1500, // optional, integer
